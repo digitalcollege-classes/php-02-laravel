31f65c61bc9f528f0d5da319e35ecb89f19a87d8
alessandrofeitoza01@gmail.com <alessandrofeitoza01@gmail.com>
2024-12-10 21:56:47 -0300

    Inicio da api de Lanchonetes

---
+++ app/Http/Controllers/Api/StoreApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+
+class StoreApiController extends Controller
+{
+    public function getList(): Response
+    {
+
+        return new Response([
+            ['id' => 1, 'name' => 'Acai da Val'],
+            ['id' => 2, 'name' => 'Pastel e Cia'],
+        ]);
+    }
+
+    public function getOne(string $id): Response
+    {
+        return new Response([
+            'id' => $id,
+            'name' => 'TEste',
+        ]);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update(string $id)
+    {
+        
+    }
+
+    public function remove(string $id)
+    {
+
+    }
+}
