f5806c37b0d37242d88578eccb792dc473bb0bc1
smz1n <samuel.gomesv2@gmail.com>
2025-01-07 21:20:54 -0300

    amend

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Adress extends Model{
+class Address extends Model{
 
     use HasFactory; //trait
 
     protected $fillable = [
-        'id',
         'street',
         'number',
         'neighborhood',
--- database/migrations/2025_01_07_231601_address.php
+++ database/migrations/2025_01_07_231601_address.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Adress', function (Blueprint $table) {
+        Schema::create('Address', function (Blueprint $table) {
             $table->id();
             $table->string('street');
             $table->string('number');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Adress'); 
+        Schema::dropIfExists('Address'); 
     }
 };
